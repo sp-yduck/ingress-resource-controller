0a046c327d32a8ca7e8a812efdfafadfcb329340
sp-yduck <sudo20.t.ab@gmail.com>
2023-02-07 01:13:26 +0900

    update service reconciler

--- controllers/service_controller.go
+++ controllers/service_controller.go
@@ -19,10 +19,15 @@ package controllers
 import (
 	"context"
 
+	v1 "k8s.io/api/core/v1"
+	networkingv1 "k8s.io/api/networking/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	networkingv1beta1 "github.com/sp-yduck/ingress-resource-controller/api/v1beta1"
 )
 
 // ServiceReconciler reconciles a Service object
@@ -45,17 +50,74 @@ type ServiceReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.12.1/pkg/reconcile
 func (r *ServiceReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	logger := log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	var svc v1.Service
+	if err := r.Get(ctx, req.NamespacedName, &svc); err != nil {
+		logger.Error(err, "unable to fetch service")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	if err := r.reconcileService(ctx, svc); err != nil {
+		logger.Error(err, "unable to reconcile service")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
 
 	return ctrl.Result{}, nil
+}
+
+func (r *ServiceReconciler) reconcileService(ctx context.Context, service v1.Service) error {
+	logger := log.FromContext(ctx)
+
+	ingressResource := &networkingv1beta1.IngressResource{}
+	ingressResource.SetName(service.Name)
+	ingressResource.SetNamespace(service.Namespace)
+	// ingressResource.SetAnnotations()
+
+	hostNameSuffix := "localhost"
+	hostName := service.Name + "." + service.Namespace + "." + hostNameSuffix
+	defaultPathType := networkingv1.PathType("Prefix")
+	op, err := ctrl.CreateOrUpdate(ctx, r.Client, ingressResource, func() error {
+		*ingressResource.Spec.Template.Spec.IngressClassName = "nginx"
+		ingressResource.Spec.Template.Spec.Rules = []networkingv1.IngressRule{
+			{
+				Host: hostName,
+				IngressRuleValue: networkingv1.IngressRuleValue{
+					HTTP: &networkingv1.HTTPIngressRuleValue{
+						Paths: []networkingv1.HTTPIngressPath{
+							{
+								Path:     "/",
+								PathType: &defaultPathType,
+								Backend: networkingv1.IngressBackend{
+									Service: &networkingv1.IngressServiceBackend{
+										Name: service.Name,
+										Port: networkingv1.ServiceBackendPort{
+											Number: service.Spec.Ports[0].Port,
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+		return ctrl.SetControllerReference(&v1.Service{}, ingressResource, r.Scheme)
+	})
+	if err != nil {
+		logger.Error(err, "unable to create or update IngressResource")
+		return err
+	}
+	if op != controllerutil.OperationResultNone {
+		logger.Info("reconcile service successfully", "op", op)
+	}
+	return nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *ServiceReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		// Uncomment the following line adding a pointer to an instance of the controlled resource as an argument
-		// For().
+		For(&v1.Service{}).
 		Complete(r)
 }
